4c58a45ee23210a33c2c09b289ec3592285abc4d
eiwashita <erikoiwashita@gmail.com>
2026-05-04 23:44:59 -0600

    Update initial book due

--- src/components/MyBooks.jsx
+++ src/components/MyBooks.jsx
@@ -32,7 +32,7 @@ const MyBooks = ({
       const sampleBook = {
         id: 'zr5NBldVA5UC',
         status: 'Currently Reading',
-        date: { start: '2026-03-01', due: '2026-04-30' },
+        date: { start: '2026-05-01', due: '2026-06-30' },
         finished: 20,
         notes:
           "- You can add your own notes here!\n- Change status from 'Currently Reading' to 'Want to Read', 'Read' or 'Remove'\n- Click 'More' for more details",
